8ca63c3e673445c49d7f1ba18fb9dc91a965fcd9
Paulo Amaral <paulo.amaral@gmail.com>
2021-05-07 18:18:31 +0200

    moved run.py to outside of the original jira-agile-metrics repo

---
+++ run.py
@@ -0,0 +1,11 @@
+import pathlib
+import sys
+import os
+
+agilemetricspath = str(pathlib.Path(__file__).parent.resolve())
+path = os.path.join(agilemetricspath, 'jira-agile-metrics')
+
+sys.path.insert(0, path)
+from jira_agile_metrics.cli import main
+main()
+sys.path.remove(path)
